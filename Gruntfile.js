85841b2da1e3c014384942af1113dfacb2861045
mcfarljw <mcfarljw@gmail.com>
2014-12-29 16:43:19 -0500

    Removed grunt-mkdir plugin in favor of native grunt file utilities.

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-yuidoc');
-    grunt.loadNpmTasks('grunt-mkdir');
     grunt.loadNpmTasks('grunt-react');
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-shell');
