f05f434abe300bd0dd22731ce40accb24eb70662
mcfarljw <mcfarljw@gmail.com>
2014-12-31 10:51:17 -0500

    Update gelato variables directly inside the gruntfile when creating project.

--- bin/cli-create.js
+++ bin/cli-create.js
@@ -8,7 +8,6 @@ var shell = require('shelljs');
 program.parse(process.argv);
 
 if (program.args.length) {
-    process.env.projectPath = gelato.project.path + '/' + program.args[0];
     if (fs.existsSync(gelato.project.path + '/' + program.args[0])) {
         console.log('Project directory already exists.');
         process.exit(1);
