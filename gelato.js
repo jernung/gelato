0333761d118823dbcfcfeccb3fc374661ae3ace5
mcfarljw <mcfarljw@gmail.com>
2014-12-30 22:17:25 -0500

    Added centralized gelato module for exporting global project variables.

---
+++ gelato.js
@@ -0,0 +1,39 @@
+var gelatoPath = process.env.gelatoPath = process.env.gelatoPath || __dirname.toString();
+var projectPath = process.env.projectPath = process.env.projectPath || process.cwd().toString();
+var fs = require('fs');
+var shell = require('shelljs');
+var gelato, project = {};
+
+try {
+    gelato = require(gelatoPath + '/package.json');
+} catch (error) {
+    console.log('Unable to load gelato package file.', error.code);
+    process.exit(1);
+}
+
+if (fs.existsSync(projectPath + '/package.json')) {
+    project = require(projectPath + '/package.json');
+}
+
+shell.cd(gelatoPath);
+
+module.exports = {
+    crosswalk: {
+        path: gelatoPath + '/cordova/crosswalk',
+        version: '10.39.235.14'
+    },
+    framework: {
+        path: gelatoPath + '/framework'
+    },
+    path: gelatoPath,
+    project: {
+        name: project.name,
+        path: projectPath,
+        type: project.type,
+        version: project.version
+    },
+    structures: {
+        path: gelatoPath + '/structures'
+    },
+    version: gelato.version
+};
