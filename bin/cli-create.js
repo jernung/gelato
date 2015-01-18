d05e5715523fc1eb8aadfa0dbab64dc3f27a95d0
mcfarljw <mcfarljw@gmail.com>
2015-01-18 15:30:10 -0600

    Refactored gelato module into globals containing all static paths.

--- bin/cli-create.js
+++ bin/cli-create.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 
-var gelato = require('../gelato.js');
+var globals = require('../globals.js');
 var fs = require('fs');
 var program = require('commander');
 var shell = require('shelljs');
-var settings = gelato.load().getSettings();
 
 program.parse(process.argv);
 
 if (program.args.length) {
     var projectName = program.args[0];
-    if (fs.existsSync(settings.project.path + '/' + projectName)) {
+    if (fs.existsSync(globals.project.path + '/' + projectName)) {
         console.log('Project directory already exists.');
         process.exit(1);
     } else {
---
+++ globals.js
@@ -0,0 +1,80 @@
+var crosswalkVersion, gelato, gelatoPath, gelatoPkg, projectPkg, projectPath;
+var fs = require('fs');
+var shell = require('shelljs');
+
+crosswalkVersion = '10.39.235.16';
+gelatoPath = process.env.gelatoPath = process.env.gelatoPath || __dirname.toString();
+projectPath = process.env.projectPath = process.env.projectPath || process.cwd().toString();
+
+
+if (fs.existsSync(gelatoPath + '/package.json')) {
+    gelatoPkg = require(gelatoPath + '/package.json');
+} else {
+    console.log('Unable to load gelato package file.', error.code);
+    process.exit(1);
+}
+
+
+if (fs.existsSync(projectPath + '/package.json')) {
+    projectPkg = require(projectPath + '/package.json');
+} else {
+    projectPkg = {}
+}
+
+
+shell.cd(gelatoPath);
+
+
+module.exports = {
+    gelato: {
+        copy: {
+            path: gelatoPath + '/copy'
+        },
+        cordova: {
+            crosswalk: {
+                arm: {
+                    path: gelatoPath + '/cordova/crosswalk/crosswalk-cordova-' + crosswalkVersion + '-arm'
+                },
+                path: gelatoPath + '/cordova/crosswalk',
+                version: crosswalkVersion,
+                x86: {
+                    path: gelatoPath + '/cordova/crosswalk/crosswalk-cordova-' + crosswalkVersion + '-x86'
+                }
+            },
+            path: gelatoPath + '/cordova',
+            plugins: {
+                path: gelatoPath + '/cordova/plugins'
+            }
+        },
+        framework: {
+            path: gelatoPath + '/framework'
+        },
+        path: gelatoPath,
+        pkg: gelatoPkg
+    },
+    project: {
+        cordova: {
+            path: projectPath + '/cordova',
+            platforms: {
+                android: {
+                    cordovalib: {
+                        path: projectPath + '/cordova/platforms/android/CordovaLib'
+                    },
+                    path: projectPath + '/cordova/platforms/android'
+                },
+                path: projectPath + '/cordova/platforms'
+            },
+            www: {
+                path: projectPath + '/cordova/www'
+            }
+        },
+        path: projectPath,
+        pkg: projectPkg,
+        src: {
+            path: projectPath + '/src'
+        },
+        www: {
+            path: projectPath + '/www'
+        }
+    }
+};
