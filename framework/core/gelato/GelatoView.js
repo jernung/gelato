c282ca93f5fdc5b94bd9b016cd7dc08209520953
mcfarljw <mcfarljw@gmail.com>
2015-01-30 12:27:01 -0600

    Added binding view event for handling link click routing.

--- framework/core/gelato/GelatoView.js
+++ framework/core/gelato/GelatoView.js
@@ -14,7 +14,22 @@ define([], function() {
          */
         renderTemplate: function(template) {
             this.$el.html(Handlebars.compile(template)(app.strings));
+            this.$('.navigate').on('vclick', this.handleNavigateClicked);
             return this;
+        },
+        /**
+         * @method handleNavigateClicked
+         * @param {Event} event
+         */
+        handleNavigateClicked: function(event) {
+            event.preventDefault();
+            var url = $(event.currentTarget).data('url').replace('#', app.isLocal() ? '/#' : '');
+            var replace = $(event.currentTarget).data('replace');
+            var trigger = $(event.currentTarget).data('trigger');
+            app.router.navigate(url, {
+                replace: replace === undefined ? false : replace,
+                trigger: trigger === undefined ? true : trigger
+            });
         }
     });
 
