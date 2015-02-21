44e1dbde9a5d40b86a2c9469eaeaee365e7bcb25
mcfarljw <mcfarljw@gmail.com>
2015-02-20 22:20:28 -0600

    Added flexible logic for handling dialogs embedded in page views.

--- framework/core/modules/GelatoDialog.js
+++ framework/core/modules/GelatoDialog.js
@@ -6,33 +6,109 @@ define([
 ], function(GelatoView) {
 
     /**
-     * @class GelatoSidebar
+     * @class GelatoDialog
      * @extends GelatoView
      */
-    var GelatoSidebar = GelatoView.extend({
+    var GelatoDialog = GelatoView.extend({
         /**
          * @method initialize
          * @constructor
          */
         initialize: function(options) {
-            options = options === undefined ? {} : options;
-            this.element = options.name === undefined ? $(this.$('.gelato-sidebar').get(0)) : this.$('#sidebar-' + options.name);
+            options = options || {};
+            this.element = null;
             this.page = options.page;
+            this.state = 'hidden';
         },
         /**
          * @property events
          * @type Object
          */
-        events: {},
+        events: {
+            'vclick [data-dialog]': 'handleClickDialogToggle'
+        },
         /**
-         * @method getName
-         * @returns {String}
+         * @method handleClickDialogToggle
+         * @param {Event} event
          */
-        getName: function() {
-            return this.element.get(0).id.replace('sidebar-', '');
+        handleClickDialogToggle: function(event) {
+            event.preventDefault();
+            var options = {};
+            var target = $(event.currentTarget);
+            var name = target.data('dialog');
+            //TODO: parse keyboard, show and remote as boolean
+            options.backdrop = target.data('backdrop');
+            options.keyboard = target.data('keyboard');
+            options.show = target.data('show');
+            options.remote = target.data('remote');
+            this.show(name, options);
+        },
+        /**
+         * @method handleModalHide
+         * @param {Event} event
+         */
+        handleModalHide: function(event) {
+            this.state = 'hide';
+            this.trigger('hide', event);
+        },
+        /**
+         * @method handleModalHidden
+         * @param {Event} event
+         */
+        handleModalHidden: function(event) {
+            this.state = 'hidden';
+            this.element = null;
+            $(event.target).find('*').off();
+            this.trigger('hidden', event);
+        },
+        /**
+         * @method handleModalShow
+         * @param {Event} event
+         */
+        handleModalShow: function(event) {
+            this.state = 'show';
+            this.trigger('show', event);
+        },
+        /**
+         * @method handleModalShown
+         * @param {Event} event
+         */
+        handleModalShown: function(event) {
+            this.state = 'shown';
+            this.trigger('shown', event);
+        },
+        /**
+         * @method hide
+         * @returns {GelatoDialog}
+         */
+        hide: function() {
+            this.element.modal('hide');
+            return this;
+        },
+        /**
+         * @method show
+         * @param {String} name
+         * @param {Object} [options]
+         * @returns {GelatoDialog}
+         */
+        show: function(name, options) {
+            options = options || {};
+            options.backdrop = options.backdrop || 'static';
+            options.keyboard = options.keyboard || false;
+            options.show = options.show || true;
+            options.remote = options.remote || false;
+            if (this.state === 'hidden') {
+                this.element = this.$('#' + name + ' .modal');
+                this.element.one('show.bs.modal', $.proxy(this.handleModalShow, this));
+                this.element.one('shown.bs.modal', $.proxy(this.handleModalShown, this));
+                this.element.one('hide.bs.modal', $.proxy(this.handleModalHide, this));
+                this.element.one('hidden.bs.modal', $.proxy(this.handleModalHidden, this));
+                this.element.modal(options);
+            }
+            return this;
         }
     });
 
-    return GelatoSidebar;
+    return GelatoDialog;
 
 });
