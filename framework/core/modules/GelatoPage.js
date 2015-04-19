4d3788a18821c7807abd577fbd76b3d63f48f31a
mcfarljw <mcfarljw@gmail.com>
2015-04-19 15:02:35 +0900

    Dynamically adjust application padding based on navbar height.

--- framework/core/modules/GelatoPage.js
+++ framework/core/modules/GelatoPage.js
@@ -21,6 +21,35 @@ define([
          */
         title: i18n.global.title,
         /**
+         * @method renderTemplate
+         * @param {String} template
+         * @returns {GelatoView}
+         */
+        renderTemplate: function(template) {
+            GelatoView.prototype.renderTemplate.call(this, template);
+            this.adjustNavbarPadding();
+            return this;
+        },
+        /**
+         * @method adjustNavbarPadding
+         * @returns {GelatoPage}
+         */
+        adjustNavbarPadding: function() {
+            var navbarFixedBottom = this.$('.navbar-fixed-bottom');
+            var navbarFixedTop = this.$('.navbar-fixed-top');
+            if (navbarFixedBottom) {
+                this.$el.css('padding-bottom', navbarFixedBottom.height());
+            } else {
+                this.$el.css('padding-bottom', '');
+            }
+            if (navbarFixedTop) {
+                this.$el.css('padding-top', navbarFixedTop.height());
+            } else {
+                this.$el.css('padding-top', '');
+            }
+            return this;
+        },
+        /**
          * @method getName
          * @returns {String}
          */
