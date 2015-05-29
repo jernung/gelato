28f44ab04fda5e2c128a0652580e76056841af77
mcfarljw <mcfarljw@gmail.com>
2015-05-29 10:44:47 -0500

    Removed unused grunt dependency for grunt-contrib-concat.

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,6 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-coffee');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-csslint');
