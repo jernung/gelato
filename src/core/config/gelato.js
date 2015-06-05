f30529747716b2a8dee437ea503093bba29152f1
mcfarljw <mcfarljw@gmail.com>
2015-06-04 23:08:20 -0500

    Updated core backbone library to version 1.2.1.

--- src/core/config/gelato.js
+++ src/core/config/gelato.js
@@ -31,7 +31,7 @@ gelato = (function() {
         paths: {
             //framework libraries
             async: 'core/libraries/async-1.0.0',
-            backbone: 'core/libraries/backbone-1.2.0',
+            backbone: 'core/libraries/backbone-1.2.1',
             'backbone.routefilter': 'core/libraries/backbone.routefilter-0.2.0',
             bootstrap: 'core/libraries/bootstrap-3.3.4',
             'bootstrap.jasny': 'core/libraries/bootstrap.jasny-3.1.3',
