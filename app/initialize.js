a702d99c4dcd7516db4397359fd68225173a9cfa
mcfarljw <mcfarljw@gmail.com>
2015-07-25 14:02:09 -0500

    Initialize application ready based on location protocol.

--- app/initialize.js
+++ app/initialize.js
@@ -2,9 +2,15 @@ var Application = require('application');
 
 module.exports = (function() {
 
-    $(document).ready(function(){
+    function start() {
         window.app = new Application();
         window.app.start();
-    });
+    }
+
+    if (location.protocol === 'file:') {
+        document.addEventListener('deviceready', start, false);
+    } else {
+        $(document).ready(start);
+    }
 
 })();
