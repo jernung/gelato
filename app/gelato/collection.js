25366e1ade6449a32fa0e67a3698aa538a033f4b
mcfarljw <mcfarljw@gmail.com>
2015-08-14 15:17:57 -0500

    Add state property to collections and models with triggers.

--- app/gelato/collection.js
+++ app/gelato/collection.js
@@ -2,4 +2,48 @@
  * @class GelatoCollection
  * @extends {Backbone.Collection}
  */
-module.exports = Backbone.Collection.extend({});
+module.exports = Backbone.Collection.extend({
+    /**
+     * @method constructor
+     * @constructor
+     */
+    constructor: function() {
+        Backbone.Collection.apply(this, arguments);
+    },
+    /**
+     * @method fetch
+     * @param {Object} [options]
+     */
+    fetch: function(options) {
+        options = options || {};
+        this.state = 'fetching';
+        this.trigger('state', 'fetching', this);
+        this.trigger('state:fetching', this);
+        options.error = this._onRequestError.bind(this);
+        options.success = this._onRequestSuccess.bind(this);
+        return Backbone.Collection.prototype.fetch.call(this, options);
+    },
+    /**
+     * @property standby
+     * @type {String}
+     */
+    state: 'standby',
+    /**
+     * @method _onRequestError
+     * @private
+     */
+    _onRequestError: function() {
+        this.state = 'standby';
+        this.trigger('state', 'standby', this);
+        this.trigger('state:standby', this);
+    },
+    /**
+     * @method _onRequestSuccess
+     * @private
+     */
+    _onRequestSuccess: function() {
+        this.state = 'standby';
+        this.trigger('state', 'standby', this);
+        this.trigger('state:standby', this);
+    }
+});
--- app/gelato/model.js
+++ app/gelato/model.js
@@ -4,12 +4,60 @@
  */
 module.exports = Backbone.Model.extend({
     /**
-     * @method equals
-     * @param {String} attribute
-     * @param {Number|String} value
-     * @returns {Boolean}
+     * @method constructor
+     * @constructor
      */
-    equals: function(attribute, value) {
-        return this.get(attribute) === value;
+    constructor: function() {
+        Backbone.Model.apply(this, arguments);
+    },
+    /**
+     * @method fetch
+     * @param {Object} [options]
+     */
+    fetch: function(options) {
+        options = options || {};
+        this.state = 'fetching';
+        this.trigger('state', 'fetching', this);
+        this.trigger('state:fetching', this);
+        options.error = this._onRequestError.bind(this);
+        options.success = this._onRequestSuccess.bind(this);
+        return Backbone.Model.prototype.fetch.call(this, options);
+    },
+    /**
+     * @method save
+     * @param [attributes]
+     * @param [options]
+     */
+    save: function(attributes, options) {
+        options = options || {};
+        this.state = 'saving';
+        this.trigger('state', 'saving', this);
+        this.trigger('state:saving', this);
+        options.error = this._onRequestError.bind(this);
+        options.success = this._onRequestSuccess.bind(this);
+        return Backbone.Model.prototype.save.call(this, attributes, options);
+    },
+    /**
+     * @property standby
+     * @type {String}
+     */
+    state: 'standby',
+    /**
+     * @method _onRequestError
+     * @private
+     */
+    _onRequestError: function() {
+        this.state = 'standby';
+        this.trigger('state', 'standby', this);
+        this.trigger('state:standby', this);
+    },
+    /**
+     * @method _onRequestSuccess
+     * @private
+     */
+    _onRequestSuccess: function() {
+        this.state = 'standby';
+        this.trigger('state', 'standby', this);
+        this.trigger('state:standby', this);
     }
 });
