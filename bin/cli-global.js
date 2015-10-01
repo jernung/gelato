c84e1e43d03b5ffd9233c7c5417a1645066e6bcd
Joshua McFarland <mcfarljw@gmail.com>
2015-10-01 16:10:43 -0500

    Rename cli-gelato to cli-global and to jasmine reference.

--- bin/cli-global.js
+++ bin/cli-global.js
@@ -4,12 +4,12 @@ var FRAMEWORK_PATH = process.env.framework.toString();
 var PROJECT_PATH = process.env.project.toString();
 var BRUNCH_PATH = FRAMEWORK_PATH + '/node_modules/.bin/brunch';
 var CORDOVA_PATH = FRAMEWORK_PATH + '/node_modules/.bin/cordova';
-var MOCHA_PATH = FRAMEWORK_PATH + '/node_modules/.bin/mocha';
+var JASMINE_PATH = FRAMEWORK_PATH + '/node_modules/.bin/jasmine';
 
 module.exports = {
     brunch: BRUNCH_PATH,
     cordova: CORDOVA_PATH,
     framework: FRAMEWORK_PATH,
-    mocha: MOCHA_PATH,
+    jasmine: JASMINE_PATH,
     project: PROJECT_PATH
 };
