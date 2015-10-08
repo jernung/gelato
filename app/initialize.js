c63f301f2b77e13cb589e3ae746a009b0c12d7f0
Joshua McFarland <mcfarljw@gmail.com>
2015-10-08 13:20:38 -0500

    Remove adding of app to the window on initialization.

--- app/initialize.js
+++ app/initialize.js
@@ -2,17 +2,9 @@ var Application = require('application');
 
 module.exports = (function() {
 
-    function prepareDOM() {
-        document.body.appendChild(document.createElement('gelato-application'));
-        document.body.appendChild(document.createElement('gelato-dialogs'));
-        document.body.appendChild(document.createElement('gelato-navbars'));
-        document.body.appendChild(document.createElement('gelato-sidebars'));
-    }
-
     function start() {
-        prepareDOM();
-        window.app = new Application();
-        window.app.start();
+        document.body.appendChild(document.createElement('gelato-application'));
+        new Application().start();
     }
 
     if (location.protocol === 'file:') {
