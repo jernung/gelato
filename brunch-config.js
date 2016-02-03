12bc2ac2c1807de3aedfb54c59e397234a09713b
Joshua McFarland <mcfarljw@gmail.com>
2016-02-03 10:50:01 -0600

    Update vendor lodash library to version 4.2.0.

--- brunch-config.js
+++ brunch-config.js
@@ -13,7 +13,7 @@ exports.config = {
             order: {
                 before: [
                     'gelato/vendor/jquery-2.2.0.js',
-                    'gelato/vendor/lodash-4.1.0.js',
+                    'gelato/vendor/lodash-4.2.0.js',
                     'gelato/vendor/backbone-1.2.3.js',
                     'vendor/bootstrap-3.3.6.js',
                     'vendor/chai-3.5.0.js',
