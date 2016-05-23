9457c329e1ce24c6898aa8de6d87dc99530cb3cb
Joshua McFarland <mcfarljw@gmail.com>
2016-05-23 17:26:34 -0500

    Update mocha test structure to match gulp build.

--- test/index.js
+++
@@ -1,8 +0,0 @@
-window.Gelato = require('gelato/global');
-
-require('bootstrap');
-require('./specs/application');
-require('./specs/dialog');
-require('./specs/collection');
-require('./specs/model');
-require('./specs/view');
--- test/specs/application.js
+++ test/specs/application.js
@@ -3,93 +3,39 @@ describe('GelatoApplication', function() {
 
   beforeEach(function() {
     application = new Gelato.Application();
+    $('body').css({height: 1000, width: 500});
   });
 
-  it('getHeight()', function() {
-    expect(application.getHeight()).to.be.above(0);
-  });
+  it(
+    'should create application elements in the body',
+    function() {
+      expect($('gelato-application')).to.have.length(1);
+      expect($('gelato-application > gelato-footer')).to.have.length(1);
+      expect($('gelato-application > gelato-modal')).to.have.length(1);
+      expect($('gelato-application > gelato-navbar')).to.have.length(1);
+      expect($('gelato-application > gelato-page')).to.have.length(1);
+    }
+  );
 
-  it('getLocalStorage()', function() {
-    localStorage.setItem('hello', JSON.stringify('world'));
-    expect(application.getLocalStorage('hello')).to.equal('world');
-  });
+  it(
+    'should be able to get the window dimensions',
+    function() {
+      expect(application.getWidth()).to.equal(500);
+      expect(application.getHeight()).to.equal(1000);
+    }
+  );
 
-  it('getPlatform()', function() {
-    expect(application.getPlatform()).to.equal('Website');
-    window.device = {platform: 'Android'};
-    expect(application.getPlatform()).to.equal('Android');
-    window.device = {platform: 'iOS'};
-    expect(application.getPlatform()).to.equal('iOS');
-  });
-
-  it('getSetting()', function() {
-    localStorage.setItem('application-hello', JSON.stringify('world'));
-    expect(application.getSetting('hello')).to.equal('world');
-  });
-
-  it('getWidth()', function() {
-    expect(application.getWidth()).to.be.above(0);
-  });
-
-  it('isAndroid()', function() {
-    window.device = {platform: 'Android'};
-    expect(application.isAndroid()).to.be.true;
-  });
-
-  it('isIOS()', function() {
-    window.device = {platform: 'iOS'};
-    expect(application.isIOS()).to.be.true;
-  });
-
-  it('isLandscape()', function() {
-    Backbone.$('gelato-application').height(300);
-    Backbone.$('gelato-application').width(400);
-    expect(application.isLandscape()).to.be.true;
-  });
-
-  it('isPortrait()', function() {
-    Backbone.$('gelato-application').height(400);
-    Backbone.$('gelato-application').width(300);
-    expect(application.isPortrait()).to.be.true;
-  });
-
-  it('isWebsite()', function() {
-    window.device = {platform: 'Website'};
-    expect(application.isWebsite()).to.be.true;
-  });
-
-  it('locale()', function() {
-    expect(application.locale('language')).to.equal('default');
-  });
-
-  it('removeLocalStorage()', function() {
-    localStorage.setItem('hello', JSON.stringify('world'));
-    application.removeLocalStorage('hello', 'world');
-    expect(JSON.parse(localStorage.getItem('application-hello'))).to.be.null;
-  });
-
-  it('removeSetting()', function() {
-    localStorage.setItem('application-hello', 'world');
-    application.removeSetting('hello', 'world');
-    expect(localStorage.getItem('application-hello')).to.be.null;
-  });
-
-  it('setLocalStorage()', function() {
-    application.setLocalStorage('hello', 'world');
-    expect(JSON.parse(localStorage.getItem('hello'))).to.equal('world');
-  });
-
-  it('setSetting()', function() {
-    application.setSetting('hello', 'world');
-    expect(JSON.parse(localStorage.getItem('application-hello'))).to.equal('world');
-  });
+  it(
+    'should be able to get the window orientation',
+    function() {
+      expect(application.isLandscape()).to.be.false;
+      expect(application.isPortrait()).to.be.true;
+    }
+  );
 
   afterEach(function() {
-    Backbone.$('gelato-application').height('100%');
-    Backbone.$('gelato-application').width('100%');
-    localStorage.removeItem('application-hello');
-    localStorage.removeItem('hello');
-    window.device = undefined;
+    $('gelato-application').remove();
+    application = null;
   });
 
 });
