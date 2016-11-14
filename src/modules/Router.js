bd1b1f114945183c06dd7df2a49b7129a80a16ff
Joshua McFarland <mcfarljw@gmail.com>
2016-11-14 12:06:39 +0800

    Move query string function to router class

--- src/modules/Router.js
+++ src/modules/Router.js
@@ -10,6 +10,28 @@ class GelatoRouter extends Backbone.Router {
     this.trigger('navigate:after', args, name);
   }
 
+  getQueryString() {
+    const location = window.location;
+
+    let query = '';
+    if (location.hash.length) {
+      query = location.hash.substring(location.hash.indexOf('?') + 1);
+    } else {
+      query = location.search.substring(1);
+    }
+
+    const params = query.split('&');
+    for (let i = 0; i < params.length; i++) {
+      const pair = params[i].split('=');
+
+      if (pair[0] === name) {
+        return pair[1];
+      }
+    }
+
+    return null;
+  }
+
   isRunning() {
     return Backbone.History.started;
   }
